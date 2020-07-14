991c6d539e68db80709d1cb7e9bdd3d2a409218d
Marco Túlio Candeo <marcotulionote@gmail.com>
2020-07-13 23:51:34 -0300

    feat(api): created trips model

---
+++ api/src/models/Trips.js
@@ -0,0 +1,28 @@
+import { Schema, model } from 'mongoose';
+import mongooseDelete from 'mongoose-delete';
+
+const Trip = new Schema(
+  {
+    from: String,
+    to: String,
+    started_at: Date,
+    finished_at: Date,
+    payment_at: Date,
+    payment_method: String,
+    value: Number,
+    user_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'users',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    selectPopulatedPaths: true,
+    toJSON: { virtuals: true },
+  },
+);
+
+Trip.plugin(mongooseDelete);
+
+export default model('trips', Trip);
