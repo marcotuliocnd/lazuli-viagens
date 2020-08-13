af789cf2b33d33ea2b8bb89772179285463540e4
Marco Tulio Candeo <marcotulionote@gmail.com>
2020-08-13 11:53:07 -0300

    fixed favicon

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: "http://localhost:3333"
+  production: true,
+  // apiUrl: "http://localhost:3333"
+  apiUrl: "http://162.241.98.238:3333"
 };
