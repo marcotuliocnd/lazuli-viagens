6d8579cc56c8c1c493f7111c515773ab1f71d564
Marco Tulio Candeo <marcotulionote@gmail.com>
2020-08-18 22:01:40 -0300

    fixed cert path

--- api/src/app.js
+++ api/src/app.js
@@ -33,8 +33,8 @@ app.use('/public', express.static(path.join(__dirname, '..', 'public')));
  */
 if (process.env.NODE_ENV === 'PRODUCTION') {
   https.createServer({
-    key: fs.readFileSync('/home/seguid/ssl/certs/seguidororganico_com_br_f479d_260f7_1605568481_b71f8e4b6543bebe69434b0abc2043aa.crt'),
-    cert: fs.readFileSync('/home/seguid/ssl/keys/f479d_260f7_1fec9854bdb81daf7fd46281711567f2.key'),
+    key: fs.readFileSync('./server.crt'),
+    cert: fs.readFileSync('./server.key'),
   }, app).listen(process.env.APP_PORT, () => {
     console.log(`> Server is running on port ${process.env.APP_PORT} in mode ${process.env.NODE_ENV}`);
   });
