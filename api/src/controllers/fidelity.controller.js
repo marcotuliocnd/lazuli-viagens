9a8de5a7e7d41e4b0af1a4c5f47dc56b04a64213
Marco Tulio Candeo <marcotulionote@gmail.com>
2020-09-05 18:26:56 -0300

    feat(fidelity); added fidelity schedule

--- api/src/controllers/fidelity.controller.js
+++ api/src/controllers/fidelity.controller.js
@@ -1,3 +1,4 @@
+import Users from '../models/User';
 import Fidelity from '../models/Fidelity';
 
 export default class RoleController {
@@ -32,6 +33,50 @@ export default class RoleController {
       return res
         .status(500)
         .json({ success: false, message: 'Internal Server Error' });
+    }
+  }
+
+  async matchingFidelity(req, res, next, matchByFn = false) {
+    try {
+      const users = await Users.find();
+
+      console.log(`> Processing ${users.length} users!`)
+      
+      let index = 0;
+      for (const user of users) {
+        console.log(`> Processing ${index++}/${users.length}!`)
+        const value = parseInt(user.value, 10);
+
+        if (value < 300) {
+          user.fidelity_level = 1;
+        } else if (value >= 300 && value < 600) {
+          user.fidelity_level = 2;
+        } else if (value >= 600 && value < 1200) {
+          user.fidelity_level = 3;
+        } else if (value < 2400 && value >= 1200) {
+          user.fidelity_level = 4;
+        } else if (value >= 2400) {
+          user.fidelity_level = 5;
+        } else {
+          user.fidelity_level = 1;
+        }
+
+        await user.save();
+      }
+
+      console.log(`> Finished`)
+
+      if (!matchByFn) {
+        const data = await Users.find();
+
+        return res.json({ success: true, data });
+      }
+    } catch (err) {
+      console.error(err.message)
+
+      if (!matchByFn) {
+        return res.status(500).json({ success: false });
+      }
     }
   }
 }
--- api/src/routes/fidelity.routes.js
+++ api/src/routes/fidelity.routes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { schedule } from 'node-cron';
 
 import FidelityController from '../controllers/fidelity.controller';
 import adminMiddleware from '../middlewares/admin.middleware';
@@ -9,5 +10,10 @@ const fidelityController = new FidelityController();
 routes.post('/', adminMiddleware, fidelityController.store);
 
 routes.get('/', adminMiddleware, fidelityController.list);
+
+schedule('50 6 * * *', async () => {
+  await fidelityController.matchingFidelity(null, null, null, true);
+}, null, true, 'America/Sao_Paulo');
+
 
 export default routes;
